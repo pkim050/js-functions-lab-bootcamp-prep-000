bc179f829ce57ffeb233a2a3eee1a85374b3e39b
Patrick A Kim <pkim050@gmail.com>
2018-10-30 02:52:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  var holiday = holiday;
-  var name = name;
-  return 'Happy ${holiday}, ${name}!';
+  return "Happy $(holiday), $(name)!";
 }
 
 function holidayCountdown(holiday, days) {
